5575ec1c6e940eb4cabe701d70955085ae50dbba
olavsro <olavsie@hotmail.com>
2025-01-12 15:12:49 +0100

    Edit go-program to share a variable properly (ex 4)

--- 1 Concurrency Essentials/shared variable/go/foo.go
+++ 1 Concurrency Essentials/shared variable/go/foo.go
@@ -3,38 +3,74 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
 )
 
-var i = 0
+type request struct{
+    action string
+    respServer chan int
+}
 
-func incrementing() {
+func server(reqChan chan request){
+    var i = 0
+    for{
+        select{
+        case req := <- reqChan:
+            switch req.action{
+            case "incr":
+                i++
+            case "dec":
+                i--
+            case "get":
+                req.respServer <- i
+            }
+    
+        }
+    }
+    
+}
+
+func incrementing(reqChan chan request, doneChan chan bool) {
     //TODO: increment i 1000000 times
     for j := 0; j < 1000000; j++ {
-        i++
+        reqChan <- request{action: "incr", respServer: nil}
     }
+    doneChan <- true
 }
 
-func decrementing() {
+func decrementing(reqChan chan request, doneChan chan bool) {
     //TODO: decrement i 1000000 times
     for j := 0; j < 1000000; j++ {
-        i--
+        reqChan <- request{action: "dec", respServer: nil}
     }
+    doneChan <- true
 }
+
+
 
 func main() {
     // What does GOMAXPROCS do? What happens if you set it to 1?
     runtime.GOMAXPROCS(2)    
 	
     // TODO: Spawn both functions as goroutines
-    go incrementing()
-    go decrementing()
+    reqChan := make(chan request)
+    doneChan := make(chan bool, 2)
+
+    go server(reqChan)
+    go incrementing(reqChan, doneChan)
+    go decrementing(reqChan, doneChan)
+
+    <-doneChan
+    <-doneChan
+
+    resChan := make(chan int)
+    reqChan <- request{action: "get", respServer: resChan}
+    i := <- resChan
 
 	
     // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
     // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
+    //time.Sleep(500*time.Millisecond)
     Println("The magic number is:", i)
 }
